0dc6fa80c0b34ea69af61d589ba8817e306e7cec
João Paulo <jpresendejava@gmail.com>
2024-01-06 10:20:03 -0400

    Validações dos inputs

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -3,9 +3,25 @@ import Buttom from "../Component/Button"
 import Input from "../Component/Input"
 import logo  from "../assets/logow.png"
 import { useForm } from "react-hook-form"
+import z from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+
+const signinShema = z.object({
+    email: z.string().nonempty("Email é obrigatório.").email().toLowerCase(),
+    password: z.string().min(6, "A senha tem que ter no minimo 6 caracteres!"),
+  });
+  
 export default function Signin() {
 
-    const {register, handleSubmit} = useForm();
+
+    const {
+    register, 
+    handleSubmit, 
+    formState:{errors}, 
+} = useForm({ resolver: zodResolver(signinShema)});
+
+   
 
     function hansubform(data)
     {
@@ -20,8 +36,16 @@ export default function Signin() {
             <form onSubmit={handleSubmit(hansubform)} className=" flex flex-col justify-center gap-4 w-full ">
                 
                 
-                <Input type="email" placeholder="Email" register={register} name="email"/>
+                <Input type="email"  placeholder="Email" register={register} name="email"/>
+               
+                {console.log(errors)}
+
+                { errors.email && <span className="text-white bg-red-700 p-4">{errors.email.message}</span>}
+
+              
                 <Input type="password" placeholder="Password" register={register} name="password"/>
+                { errors.password && <span className="text-white bg-red-700 p-4">{errors.password.message}</span>}
+
                 <Buttom type="submit" text="SIGNIN"/>
                 
             </form>
