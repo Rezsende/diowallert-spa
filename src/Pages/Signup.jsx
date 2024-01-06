62bfa701373968cb02d0f0e313e8cd362df6a77d
João Paulo <jpresendejava@gmail.com>
2024-01-06 10:37:28 -0400

    Validações inpur siginup

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -4,11 +4,40 @@ import Input from "../Component/Input"
 import logo  from "../assets/logow.png"
 import { useForm } from "react-hook-form"
 import { BiArrowBack } from "react-icons/bi";
+import z from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+
+
+const signinUpShema = z.object({
+    name:z.string().min(3, "O nome precisar ter no minimo 3 caracteres!")
+    .transform((name)=>{
+        return name
+        .trim().split(" ").map((word)=>{
+            return word[0].toLocaleUpperCase().concat(word.substring(1));
+        }).join(" ");
+    }),
+    email: 
+    z.string()
+    .email("Email invalido.")
+    .nonempty("O Email é obrigatório.")
+    .toLowerCase(),
+    password: z.string().min(6, "A senha tem que ter no minimo 6 caracteres!"),
+    ConfirmPassword: z.string
+
+
+  });
+
+
 
 export default function Signup() {
 
 
-    const {register, handleSubmit} = useForm();
+    const {register, 
+        handleSubmit,
+        formState:{errors}, 
+    
+    } = useForm({ resolver: zodResolver(signinUpShema)});
 
     function hansubform(data)
     {
@@ -29,10 +58,15 @@ export default function Signup() {
             <form onSubmit={handleSubmit(hansubform)} className=" flex flex-col justify-center gap-4 w-full ">
                 
                 <Input type="name" placeholder="Name" register={register} name="name"/> 
+                { errors.email && <span className="text-white bg-red-700 p-4">{errors.email.message}</span>}
                 <Input type="email" placeholder="Email" register={register} name="email"/>
+                { errors.email && <span className="text-white bg-red-700 p-4">{errors.email.message}</span>}
                 <Input type="password" placeholder="Password" register={register} name="password"/>
-                <Input type="password" placeholder="PasswordConfirm" register={register} name="passwordConfirm"/>
+                { errors.email && <span className="text-white bg-red-700 p-4">{errors.email.message}</span>}
+                <Input type="password" placeholder="ConfirmPassword" register={register} name="ConfirmPassword"/>
+                { errors.email && <span className="text-white bg-red-700 p-4">{errors.email.message}</span>}
                 <Buttom type="submit" text="Register"/>
+
                 
             </form>
           
